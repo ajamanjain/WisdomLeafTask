c5b7a2b97990ade8d5b4fb381adf9e024093775c
Aman Jain <ajjaina.jain@gmail.com>
2023-05-14 00:03:05 +0530

    first commit

---
+++ WisdomLeafTask/WisdomLeafTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WisdomLeafTaskApp.swift
+//  WisdomLeafTask
+//
+//  Created by Aman Jain on 13/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WisdomLeafTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
